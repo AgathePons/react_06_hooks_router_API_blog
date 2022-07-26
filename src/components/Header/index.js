09b8c2bb58a60e42b80f3fb7d43c40e867ed2371
AgathePons <ponsagathe37@gmail.com>
2022-07-26 18:05:17 +0200

    Header done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,36 @@
+// == Import
+import PropTypes from 'prop-types';
 import './styles.scss';
 
-const Header = () => (
-  <header className="menu">
-    <nav>
-      <a className="menu-link menu-link--selected" href="">Accueil</a>
-      <a className="menu-link" href="">React</a>
-      <a className="menu-link" href="">Angular</a>
-      <button className="menu-btn" type="button">Activer le mode zen</button>
-    </nav>
-  </header>
-);
+// == Composant
+function Header({ categories }) {
+  return (
+    <header className="menu">
+      <nav>
+        {
+          categories.map((category) => (
+            <a
+              className={category.route === '/' ? 'menu-link menu-link--selected' : 'menu-link'}
+              href=""
+              key={category.route}
+            >
+              {category.label}
+            </a>
+          ))
+        }
+        <button className="menu-btn" type="button">Activer le mode zen</button>
+      </nav>
+    </header>
+  );
+}
+
+Header.propTypes = {
+  categories: PropTypes.arrayOf(
+    PropTypes.shape({
+      route: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
 
 export default Header;
--- src/components/Posts/index.js
+++ src/components/Posts/index.js
@@ -1,19 +1,35 @@
+// == Import
+import PropTypes from 'prop-types';
 import Post from 'src/components/Post';
 
 import './styles.scss';
 
-const Posts = () => (
-  <main className="posts">
-    <h1 className="posts-title">Dev Of Thrones</h1>
-    <div className="posts-list">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-    </div>
-  </main>
-);
+function Posts({ posts }) {
+  return (
+    <main className="posts">
+      <h1 className="posts-title">Dev Of Thrones</h1>
+      <div className="posts-list">
+        {
+          posts.map((post) => (
+            <Post
+              key={post.id}
+            />
+          ))
+        }
+      </div>
+    </main>
+  );
+}
+
+Posts.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      excerpt: PropTypes.string.isRequired,
+    }).isRequired,
+  ).isRequired,
+};
 
 export default Posts;
