3a2ea77a37518864879c4186d4257be512cf871e
AgathePons <ponsagathe37@gmail.com>
2022-07-27 10:27:18 +0200

    first Hook Yayyy git add .

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 // Composants
 import Header from 'src/components/Header';
 import Posts from 'src/components/Posts';
@@ -10,35 +10,28 @@ import postsData from 'src/data/posts';
 import './styles.scss';
 
 // == Composant
-class Blog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isZenMode: false,
-    };
+function Blog() {
+  // first element, name of the state variable
+  // second element, name of the function which allow to modify the value of the state
+  // so useState return an array with the current value of the state variable,
+  // and the function to modify it
+  const [isZenMode, setIsZenMode] = useState(false);
 
-    this.handleButtonZenChange = this.handleButtonZenChange.bind(this);
-  }
+  const handleZenButtonClick = () => {
+    setIsZenMode(!isZenMode);
+  };
 
-  handleButtonZenChange() {
-    const { isZenMode } = this.state;
-    this.setState({ isZenMode: !isZenMode });
-  }
-
-  render() {
-    const { isZenMode } = this.state;
-    return (
-      <div className="blog">
-        <Header
-          categories={categoriesData}
-          isZenMode={isZenMode}
-          onButtonZenChange={this.handleButtonZenChange}
-        />
-        <Posts posts={postsData} isZenMode={isZenMode} />
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div className="blog">
+      <Header
+        categories={categoriesData}
+        isZenMode={isZenMode}
+        onButtonZenChange={handleZenButtonClick}
+      />
+      <Posts posts={postsData} isZenMode={isZenMode} />
+      <Footer />
+    </div>
+  );
 }
 
 // == Export
